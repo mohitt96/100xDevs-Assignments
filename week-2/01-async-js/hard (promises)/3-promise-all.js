fabb53a4b5f2b9ff14bcd2204b749e649b18daf0
Mohit Tulsani <m.tulsani@PCMOHTULM2.local>
2024-01-05 16:05:08 +0530

    Promises hard assignments

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,44 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve()
+        }, t*1000)
+    })
 }
 
 function wait2(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve()
+        }, t*1000)
+    })
 }
 
 function wait3(t) {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            resolve()
+        }, t*1000)
+    })
 }
+
 
 function calculateTime(t1, t2, t3) {
-
-}
+    const promises = [
+      wait1(t1),
+      wait2(t2),
+      wait3(t3)
+    ];
+  
+    const startTime = Date.now();
+  
+    return Promise.all(promises).then(() => {
+      const endTime = Date.now();
+      const timeDifference = endTime - startTime;
+      return timeDifference;
+    });
+  }
 
 module.exports = calculateTime;
